02b2315f290039e65fd5e4aa6232724c6e2a20f6
Rohit Soni <92707347+rohitsoniinfo@users.noreply.github.com>
2023-09-16 22:57:00 +0530

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-cd /home/kali/WifiPentesting/testingSomething
-echo "enter the file path of captured file "
-read FILE
-echo "enter the password list"
-read PASSWORDLIST
-sudo aircrack-ng $FILE -w $PASSWORDLIST
 
+# Change to the directory where your WiFi pentesting tools are located.
+cd /home/kali/WifiPentesting/testingSomething
+
+# Prompt the user to enter the file path of the captured file (likely the output file from airodump-ng).
+echo "Enter the file path of the captured file:"
+read FILE
+
+# Prompt the user to enter the password list to be used for the attack.
+echo "Enter the password list:"
+read PASSWORDLIST
+
+# Use aircrack-ng to attempt to crack the WiFi password using the captured file and the provided password list.
+sudo aircrack-ng $FILE -w $PASSWORDLIST
