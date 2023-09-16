eda47028d7ec16c02aae8ac2efd36db68f3f10b3
Rohit Soni <92707347+rohitsoniinfo@users.noreply.github.com>
2023-09-16 22:56:12 +0530

    Update preAutomate.sh

--- preAutomate.sh
+++ preAutomate.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+
+# Change to the directory where your WiFi pentesting tools are located.
 cd /home/kali/WifiPentesting/testingSomething
-sudo airmon-ng check rfkillsudo
+
+# Check and disable any RF kill processes that might interfere with WiFi operation.
+sudo airmon-ng check rfkill
+
+# Kill processes that can interfere with WiFi operations.
 sudo airmon-ng check kill
-sudo airmon-ng start wlan0 
+
+# Start the monitor mode on the wireless interface (wlan0 in this case).
+sudo airmon-ng start wlan0
+
+# Use airodump-ng to scan and list available WiFi networks in the 5GHz band.
 sudo airodump-ng --band a wlan0
-echo "enter the channel number of target"
+
+# Prompt the user to enter the channel number of the target network.
+echo "Enter the channel number of the target:"
 read CH
-echo "enter the bssid of target"
+
+# Prompt the user to enter the BSSID (MAC address) of the target network.
+echo "Enter the BSSID of the target:"
 read BSSID
+
+# Use airodump-ng to capture packets from the target network on the specified channel and save them to a file with a prefix "psk".
 sudo airodump-ng -c $CH --bssid $BSSID wlan0 -w psk
